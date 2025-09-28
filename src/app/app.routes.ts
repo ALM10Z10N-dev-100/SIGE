14ff09290f18619703e5faa4837f0ed4be28e61e
Francisco <franalmazan17@gmail.com>
2025-09-28 08:23:03 -0600

    feat: build de Angular listo para despliegue en Vercel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,6 @@ import { CuentaActivadaComponent } from './cuenta-activada/cuenta-activada';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./bienvenida/bienvenida')
-                      .then(m => m.Bienvenida)
-  },
-  {
-    path: '',
     children: [
       {
         path: '',
@@ -35,7 +30,68 @@ export const routes: Routes = [
         loadComponent: () => import('./bienvenida/bienvenida')
                             .then(m => m.Bienvenida)
       },
-      // ... todas las demás rutas
+      {
+        path: 'quienes-somos',
+        loadComponent: () => import('./quienes-somos/quienes-somos')
+                            .then(m => m.QuienesSomos)
+      },
+      {
+        path: 'conectando-el-aprendizaje',
+        loadComponent: () => import('./conectando-el-aprendizaje/conectando-el-aprendizaje')
+                            .then(m => m.ConectandoElAprendizaje)
+      },
+      {
+        path: 'docentes',
+        loadComponent: () => import('./docentes/docentes')
+                            .then(m => m.Docentes)
+      },
+      {
+        path: 'manitas-entusiastas',
+        loadComponent: () => import('./manitas-entusiastas/manitas-entusiastas')
+                            .then(m => m.ManitasEntusiastas)
+      },
+      {
+        path: 'una-comunidad-que-inspira',
+        loadComponent: () => import('./una-comunidad-que-inspira/una-comunidad-que-inspira')
+                            .then(m => m.UnaComunidadQueInspira)
+      },
+      {
+        path: 'configuracion',
+        loadComponent: () => import('./configuracion/configuracion')
+                            .then(m => m.Configuracion)
+      },
+      {
+        path: 'recursos',
+        loadComponent: () => import('./recursos/recursos')
+                            .then(m => m.RecursosPanelComponent)
+      },
+      {
+        path: 'calendario',
+        loadComponent: () => import('./calendario/calendario.component')
+                            .then(m => m.CalendarioEmergenteComponent)
+      },
+      {
+        path: 'login',
+        loadComponent: () => import('./login.component/login.component')
+                            .then(m => m.LoginComponent)
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./dashboard/dashboard')
+                            .then(m => m.DashboardCalificaciones)
+      },
+      {
+        path: 'calificaciones-bimestre_Prim',
+        loadComponent: () => import('./calificaciones-bimestre_Prim/calificaciones-bimestre-pri')
+                            .then(m => m.CalificacionesBimestre)
+      },
+      {
+        path: 'Evaluaciones',
+        loadComponent: () => import('./evaluaciones/evaluaciones')
+                            .then(m => m.Evaluaciones)
+      },
+      { path: 'confirmacion', component: ConfirmacionComponent },
+      { path: 'cuenta-activada', component: CuentaActivadaComponent }
     ]
   }
-];
+]; 
